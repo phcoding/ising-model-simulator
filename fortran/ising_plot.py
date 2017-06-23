4e61915b1358112dbc69ef85942e3d94c6c05d17
PH <phcoding@users.noreply.github.com>
2017-06-23 20:42:17 +0800

    Add status message.

--- fortran/ising_plot.py
+++ fortran/ising_plot.py
@@ -17,16 +17,19 @@ def main():
         img_path = sys.argv[2]
     if spin_file != None:
         if os.path.isfile(spin_file):
+            print('Log: loding spin file ...')
             spin = np.loadtxt(os.path.abspath(spin_file))
+            print('Log: drwawing spin image ...')
             plb.imshow(spin)
             plb.set_cmap('gray')
-            print(img_path)
             if img_path != None:
                 try:
                     plb.imsave(img_path, spin)
+                    print('Log: img saved into `%s`.' % img_path)
                 except:
                     print('Error: img save error !')
             else:
+                print('Log: showing spin image ...')
                 plb.show()
         else:
             print('Error: no such spin file.')
