3a6676c82f22fbd5c5e9ec8951f292e44d12df44
PH <phcoding@users.noreply.github.com>
2017-07-02 15:36:30 +0800

    Add function to show energy log file.

--- fortran/ising_plot.py
+++ fortran/ising_plot.py
@@ -2,37 +2,44 @@
 
 import numpy as np
 import pylab as plb
-import sys
 import os
+import argparse
 
 def main():
-    spin_file = None
-    img_path = None
-    if len(sys.argv) < 2 or len(sys.argv) > 3:
-        print ('Usage: ising_plot [spins_file] [save image]')
-        return
-    if len(sys.argv) > 1:
-        spin_file = sys.argv[1]
-    if len(sys.argv) > 2:
-        img_path = sys.argv[2]
-    if spin_file != None:
-        if os.path.isfile(spin_file):
-            print('Log: loding spin file ...')
-            spin = np.loadtxt(os.path.abspath(spin_file))
-            print('Log: drwawing spin image ...')
-            plb.imshow(spin)
-            plb.set_cmap('gray')
-            if img_path != None:
-                try:
-                    plb.imsave(img_path, spin)
-                    print('Log: img saved into `%s`.' % img_path)
-                except:
-                    print('Error: img save error !')
-            else:
-                print('Log: showing spin image ...')
-                plb.show()
+    """
+    Draw spin file to image and display or save to image file.
+
+    Usage: `spin_plot  [-l] [fdata] [-o fsave]`
+    Params:
+        --fdata     data file from ising simulating.
+        --fsave     image file to save in.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--log", help="plot log data.", action="store_true")
+    parser.add_argument("fdata", help="data file from ising simulation.")
+    parser.add_argument("-o", "--fsave", help="image file to save in.")
+    args = parser.parse_args()
+    if not os.path.isfile(args.fdata):
+        print('Error: data file not exist !')
+    else:
+        # load data file.
+        data = np.loadtxt(args.fdata)
+
+        # plot data to image.
+        if args.log:
+            plb.plot(data[:,0], data[:,1])
         else:
-            print('Error: no such spin file.')
+            plb.imshow(data)
+            plb.set_cmap('gray')
+
+        # save image file
+        if args.fsave is not None:
+            if args.log:
+                plb.savefig(args.fsave)
+            else:
+                plb.imsave(args.fsave, data)
+        else:
+            plb.show()
 
 if __name__ == '__main__':
     main()
